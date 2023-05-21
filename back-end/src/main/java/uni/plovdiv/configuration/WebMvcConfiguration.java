debe40341e8572ca5455688587899767e3dc6898
filipdimitrovomarchevski <filipomarchevski@deltasource.eu>
2023-05-21 14:04:51 +0300

    Configure timezone

--- back-end/src/main/java/uni/plovdiv/configuration/WebMvcConfiguration.java
+++ back-end/src/main/java/uni/plovdiv/configuration/WebMvcConfiguration.java
@@ -4,11 +4,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @Configuration
 public class WebMvcConfiguration {
 
     @Bean
     public MethodValidationPostProcessor methodValidationPostProcessor() {
         return new MethodValidationPostProcessor();
+    }
+
+    @PostConstruct
+    public void setupTimezone() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
     }
 }
