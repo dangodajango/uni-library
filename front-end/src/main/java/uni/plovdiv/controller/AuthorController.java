62f601c895f53df7dac04a4503ac45b19af07f6a
filipdimitrovomarchevski <filipomarchevski@deltasource.eu>
2023-05-24 08:28:24 +0300

    Setup author edit and delete pages

--- front-end/src/main/java/uni/plovdiv/controller/AuthorController.java
+++ front-end/src/main/java/uni/plovdiv/controller/AuthorController.java
@@ -1,19 +1,25 @@
 package uni.plovdiv.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import uni.plovdiv.dto.author.FullAuthorInformationDto;
 import uni.plovdiv.service.AuthorService;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Objects;
 
 @Controller
 @RequestMapping("/authors")
+@SessionAttributes("oldAuthor")
 @RequiredArgsConstructor
 public class AuthorController {
 
@@ -38,6 +44,58 @@ public class AuthorController {
     ) {
         fullAuthorInformationDto.setAlive(isAlive);
         authorService.createAuthor(fullAuthorInformationDto);
+        return "redirect:/authors/";
+    }
+
+    @PostMapping("/find")
+    public String findAuthor(
+            @RequestParam String firstName,
+            @RequestParam String lastName,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate birthYear,
+            RedirectAttributes redirectAttributes
+    ) {
+        FullAuthorInformationDto fullAuthorInformationDto = authorService.findAuthorByFirstNameLastNameBirthYear(firstName, lastName, birthYear);
+        redirectAttributes.addFlashAttribute("author", fullAuthorInformationDto);
+        return "redirect:/authors/edit-page";
+    }
+
+    @RequestMapping("/edit/author-info")
+    public String findAuthorPage() {
+        return "edit-author-info";
+    }
+
+    @RequestMapping("/edit-page")
+    public String getEditAuthorPage(
+            @ModelAttribute("author") FullAuthorInformationDto author,
+            Model model
+    ) {
+        model.addAttribute("oldAuthor", author);
+        return "edit-author";
+    }
+
+    @PostMapping("/edit")
+    public String editAuthor(
+            @ModelAttribute("authorUpdated") FullAuthorInformationDto authorUpdated,
+            Model model
+    ) {
+        FullAuthorInformationDto oldAuthor = (FullAuthorInformationDto) Objects.requireNonNull(model.getAttribute("oldAuthor"));
+        authorService.editAuthor(oldAuthor, authorUpdated);
+        return "redirect:/authors/";
+    }
+
+
+    @RequestMapping("/delete-page")
+    public String getAuthorDeletePage() {
+        return "delete-author";
+    }
+
+    @PostMapping("/delete")
+    public String deleteAuthor(
+            @RequestParam String firstName,
+            @RequestParam String lastName,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate birthYear
+    ) {
+        authorService.deleteAuthor(firstName, lastName, birthYear);
         return "redirect:/authors/";
     }
 }
--- front-end/src/main/java/uni/plovdiv/service/AuthorService.java
+++ front-end/src/main/java/uni/plovdiv/service/AuthorService.java
@@ -10,10 +10,13 @@ import uni.plovdiv.RestClient;
 import uni.plovdiv.dto.author.AuthorInformationDto;
 import uni.plovdiv.dto.author.FullAuthorInformationDto;
 
+import java.time.LocalDate;
 import java.util.List;
 
+import static org.springframework.http.HttpMethod.DELETE;
 import static org.springframework.http.HttpMethod.GET;
 import static org.springframework.http.HttpMethod.POST;
+import static org.springframework.http.HttpMethod.PUT;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 
 @Service
@@ -47,6 +50,17 @@ public class AuthorService {
         }
     }
 
+    public FullAuthorInformationDto findAuthorByFirstNameLastNameBirthYear(String firstName, String lastName, LocalDate birthYear) {
+        return getAllAuthorsFullInformation().stream()
+                .filter(author ->
+                        author.getFirstName().equals(firstName)
+                                && author.getLastName().equals(lastName)
+                                && author.getBirthYear().equals(birthYear)
+                )
+                .findFirst()
+                .orElseThrow(IllegalStateException::new);
+    }
+
     public void createAuthor(FullAuthorInformationDto fullAuthorInformationDto) {
         try {
             String requestBody = objectMapper.writeValueAsString(fullAuthorInformationDto);
@@ -56,5 +70,26 @@ public class AuthorService {
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public void editAuthor(FullAuthorInformationDto oldAuthor, FullAuthorInformationDto updatedAuthor) {
+        try {
+            String uri = String.format("http://localhost:8081/author/update?firstName=%s&lastName=%s&birthYear=%s",
+                    oldAuthor.getFirstName(), oldAuthor.getLastName(), oldAuthor.getBirthYear());
+            HttpHeaders httpHeaders = new HttpHeaders();
+            httpHeaders.setContentType(APPLICATION_JSON);
+            String requestBody = objectMapper.writeValueAsString(updatedAuthor);
+            restClient.sendRequest(uri, PUT, httpHeaders, requestBody, String.class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void deleteAuthor(String firstName, String lastName, LocalDate birthYear) {
+        String uri = String.format("http://localhost:8081/author/delete?firstName=%s&lastName=%s&birthYear=%s", firstName, lastName, birthYear);
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(APPLICATION_JSON);
+        String emptyRequestBody = "";
+        restClient.sendRequest(uri, DELETE, httpHeaders, emptyRequestBody, String.class);
     }
 }
